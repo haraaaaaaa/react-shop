97627defc9668da0d52c7be684524288c18c873b
hara@pc <harunhebibovic05@gmail.com>
2025-02-16 01:36:38 +0100

    products list with useState

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -1,4 +1,5 @@
-import React, { useState, useReducer } from "react";
+import React, { useState, useReducer, useEffect } from "react";
+import axios from "axios";
 //import { Product } from "./Product";
 //import { products } from "../data/data";
 import Card from "../shared/UIElements/Card";
@@ -6,14 +7,23 @@ import { getProducts } from "../services/api/product-services";
 
 const ProductList = () => {
   const [products, setProducts] = useState([]);
+  const [error, setError] = useState(null);
 
-  // getProducts()
-  //   .then((data) => {
-  //     setProducts(data);
-  //   })
-  //   .catch(console.error.bind(console));
+  useEffect(() => {
+    const fetchProducts = async () => {
+      try {
+        const response = await axios.get("https://api.escuelajs.co/api/v1/products");
+        const productsData = response.data;
+        setProducts(productsData);
+      } catch (error) {
+        setError("Error while fetching data");
+      }
+    };
 
-  //console.log(products);
+    fetchProducts();
+  }, []);
+
+  console.log(products); // 2x output in console
   return (
     <Card className={`max-w`}>
       <ul>test</ul>
