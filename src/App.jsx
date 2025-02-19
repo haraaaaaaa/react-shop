d5db7256c9297fc81d9053ea3948d353a43401c9
hara@laptop <harunhebibovic05@gmail.com>
2025-02-19 23:29:00 +0100

    cart context added

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,14 @@ import { BrowserRouter as Router, Route, Routes } from "react-router";
 import NavBar from "./shared/Navigation/NavBar";
 import ProductList from "./components/ProductList";
 import ProductDetail from "./components/ProductDetail";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <Router>
       <NavBar />
       <Routes>
-        <Route exact path="/" element={<ProductList />} />
+        <Route exact path="/" element={<Home />} />
         <Route exact path="/products" element={<ProductList />} />
         <Route exact path="/products/:id" element={<ProductDetail />} />
       </Routes>
---
+++ src/components/Cart.jsx
@@ -0,0 +1,7 @@
+import React, { useState } from "react";
+
+const Cart = () => {
+  return <div>Cart</div>;
+};
+
+export default Cart;
