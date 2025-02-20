a35f66e04387ba97aa8f51741348b76f3e401720
hara@pc <harunhebibovic05@gmail.com>
2025-02-20 01:39:51 +0100

    updated cart context

--- src/context/cartContext.jsx
+++ src/context/cartContext.jsx
@@ -8,24 +8,26 @@ export const CartProvider = ({ children }) => {
   const [cart, setCart] = useState([]);
 
   const addToCart = (product) => {
-    setCart((prevCart) => [...prevCart, product]);
-    console.log("Added to cart ", product.id);
+    setCart((prevCart) => {
+      const existingProduct = prevCart.find((item) => item.id === product.id);
+
+      if (existingProduct) {
+        return prevCart.map((item) => (item.id === product.id ? { ...item, quantity: item.quantity + 1 } : item));
+      }
+
+      return [...prevCart, { ...product, quantity: 1 }];
+    });
   };
 
   const removeFromCart = (id) => {
-    setCart((prevCart) => prevCart.filter((item) => item.id !== id));
-    console.log("Removed from cart ", id);
+    setCart(
+      (prevCart) => prevCart.map((item) => (item.id === id ? { ...item, quantity: item.quantity - 1 } : item)).filter((item) => item.quantity > 0) // Remove only when quantity reaches 0
+    );
   };
 
   const clearCart = () => {
     setCart([]);
   };
 
-  return (
-    <CartContext.Provider
-      value={{ cart, addToCart, removeFromCart, clearCart }}
-    >
-      {children}
-    </CartContext.Provider>
-  );
+  return <CartContext.Provider value={{ cart, addToCart, removeFromCart, clearCart }}>{children}</CartContext.Provider>;
 };
